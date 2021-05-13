48bcc0e6b3dea42609fb71109db6bb8a302a58fa
Martin Hedberg <c17mhg@cs.umu.se>
2021-05-13 16:15:03 +0200

    Flatterm datatype

---
+++ src/matching/flatterm.c
@@ -0,0 +1,71 @@
+#include "flatterm.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+struct flatterm {
+    term* first;
+    term* end;
+};
+
+flatterm* flatterm_init() {
+    flatterm* ft = calloc(1, sizeof(flatterm));
+    if (!ft) {
+        perror("flatterm");
+        exit(errno);
+    }
+
+    return ft;
+}
+
+term* flatterm_push_back(flatterm* ft) {
+    term* t = calloc(1, sizeof(term));
+    if (!t) {
+        perror("ft_add");
+        exit(errno);
+    }
+
+	t->m_type = -1;
+	t->f_type = -1;
+	t->symbol = NULL;
+    t->end = t;
+
+    if (ft->first == NULL) {
+        ft->first = t;
+        ft->end = t;
+    } else {
+        ft->end->next = t;
+        t->prev = ft->end;
+        // t->next = NULL; Implicit by calloc
+        ft->end = t;
+    }
+
+    return t;
+}
+
+term* flatterm_first(flatterm* ft) {
+    return ft->first;
+}
+
+term* flatterm_end(flatterm* ft) {
+    return ft->end;
+}
+
+void flatterm_free(flatterm* ft) {
+    term* t = flatterm_first(ft);
+    while (t) {
+        term* next = t->next;
+        free(t);
+        t = next;
+    }
+
+    free(ft);
+}
+
+void flatterm_print(flatterm* ft) {
+    term* t = flatterm_first(ft);
+    while (t) {
+        printf("Symbol: %s, type: %s, matching: %s, end-sym: %s\n", t->symbol, (t->f_type ? "prefix" : "symbol"), (t->m_type ? "variable" : "constant"), t->end->symbol);
+        t = t->next;
+    }
+}
--- src/matching/include/flatterm.h
+++ src/matching/include/flatterm.h
@@ -1,8 +1,6 @@
 #ifndef MC_FLATTERM
 #define MC_FLATTERM
 
-#include "list.h"
-
 enum matchtype {
 	MT_CONSTANT, // 2 or f
 	MT_VARIABLE, // f_ or x_
@@ -15,13 +13,28 @@ enum functype {
 	FT_INFIX
 };
 
+typedef struct flatterm flatterm;
+
 typedef struct term {
+	struct term* next;
+	struct term* prev;
+	struct term* end;
 	enum matchtype m_type;
 	enum functype f_type;
 	char* symbol;
-	struct term* end;
 } term;
 
+flatterm* flatterm_init();
+
+term* flatterm_push_back(flatterm* ft);
+
+term* flatterm_first(flatterm* ft);
+
+term* flatterm_end(flatterm* ft);
+
+void flatterm_free(flatterm* ft);
+
+void flatterm_print(flatterm* ft);
 
 
 #endif
